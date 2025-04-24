5b97e3c07ee1dc12df2209f8e97c264ee83bafb8
Corey Wynn <coreywynn@Coreys-MacBook-Pro-2.local>
2025-04-24 11:08:49 -0500

    remove logs

--- src/lib/routes/health-check.ts
+++ src/lib/routes/health-check.ts
@@ -1,52 +1,56 @@
-import type { Request, Response } from 'express';
-import type { IUnleashConfig } from '../types/option';
-import type { IUnleashServices } from '../types/services';
-import type { Logger } from '../logger';
-import type { OpenApiService } from '../services/openapi-service';
+import type { Request, Response } from "express";
+import type { IUnleashConfig } from "../types/option";
+import type { IUnleashServices } from "../types/services";
+import type { Logger } from "../logger";
+import type { OpenApiService } from "../services/openapi-service";
 
-import Controller from './controller';
-import { NONE } from '../types/permissions';
-import { createResponseSchema } from '../openapi/util/create-response-schema';
-import type { HealthCheckSchema } from '../openapi/spec/health-check-schema';
+import Controller from "./controller";
+import { NONE } from "../types/permissions";
+import { createResponseSchema } from "../openapi/util/create-response-schema";
+import type { HealthCheckSchema } from "../openapi/spec/health-check-schema";
 
 export class HealthCheckController extends Controller {
-    private logger: Logger;
+  private logger: Logger;
 
-    private openApiService: OpenApiService;
+  private openApiService: OpenApiService;
 
-    constructor(
-        config: IUnleashConfig,
-        { openApiService }: Pick<IUnleashServices, 'openApiService'>,
-    ) {
-        super(config);
-        this.logger = config.getLogger('health-check.js');
-        this.openApiService = openApiService;
+  constructor(
+    config: IUnleashConfig,
+    { openApiService }: Pick<IUnleashServices, "openApiService">
+  ) {
+    super(config);
+    this.logger = config.getLogger("health-check.js");
+    this.openApiService = openApiService;
 
-        this.route({
-            method: 'get',
-            path: '',
-            handler: this.getHealth,
-            permission: NONE,
-            middleware: [
-                openApiService.validPath({
-                    tags: ['Operational'],
-                    operationId: 'getHealth',
-                    summary: 'Get instance operational status',
-                    description:
-                        'This operation returns information about whether this Unleash instance is healthy and ready to serve requests or not. Typically used by your deployment orchestrator (e.g. Kubernetes, Docker Swarm, Mesos, et al.).',
-                    responses: {
-                        200: createResponseSchema('healthCheckSchema'),
-                        500: createResponseSchema('healthCheckSchema'),
-                    },
-                }),
-            ],
-        });
-    }
+    // @ts-ignore
+    console.log("---start HealthCheckController---");
+    // @ts-ignore
+    console.log("config.server", config.server);
+    // @ts-ignore
+    console.log("---end HealthCheckController---");
 
-    async getHealth(
-        _: Request,
-        res: Response<HealthCheckSchema>,
-    ): Promise<void> {
-        res.status(200).json({ health: 'GOOD' });
-    }
+    this.route({
+      method: "get",
+      path: "",
+      handler: this.getHealth,
+      permission: NONE,
+      middleware: [
+        openApiService.validPath({
+          tags: ["Operational"],
+          operationId: "getHealth",
+          summary: "Get instance operational status",
+          description:
+            "This operation returns information about whether this Unleash instance is healthy and ready to serve requests or not. Typically used by your deployment orchestrator (e.g. Kubernetes, Docker Swarm, Mesos, et al.).",
+          responses: {
+            200: createResponseSchema("healthCheckSchema"),
+            500: createResponseSchema("healthCheckSchema"),
+          },
+        }),
+      ],
+    });
+  }
+
+  async getHealth(_: Request, res: Response<HealthCheckSchema>): Promise<void> {
+    res.status(200).json({ health: "GOOD" });
+  }
 }
